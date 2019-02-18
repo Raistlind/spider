ddb5021cec2c7193b6d2e26fd2b618a586ac3c34
RaistlinD <Johnd0712@gmail.com>
2019-02-18 21:13:06 +0800

    ok

--- HelloWorld.py
+++ HelloWorld.py
@@ -1,1 +1,19 @@
-print('Hello, World!')
+# print('Hello, World!')
+
+print(type (1))
+print(type(2/2))
+print(type(2//2))
+
+print(0b10)
+
+print(0b11)
+
+print(0o10)
+
+print(0o11)
+
+print(0x10)
+
+print(0x11)
+
+print(ord('a'))
