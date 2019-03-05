a9e9bd17e21ad4cdc7971e45264f08fcd1ab5fdb
RaistlinD <Johnd0712@gmail.com>
2019-03-05 22:33:14 +0800

    ok

---
+++ spider.py
@@ -0,0 +1,16 @@
+from urllib import request
+
+
+class spider():
+    url = 'https://www.panda.tv/cate/lol'
+
+    def __fetch_content(self):
+        r = request.urlopen(spider.url)
+        htmls = r.read()
+
+    def go(self):
+        self.__fetch_content()
+
+
+spider = spider()
+spider.go()
